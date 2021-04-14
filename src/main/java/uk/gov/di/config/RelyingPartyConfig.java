ea8e20e795669da26a93c6f7b55af8b52dd9df78
Sandeep Nagra <san@initialspark.co.uk>
2021-04-14 15:59:24 +0100

    Change port name so doesn't conflict with paas

--- src/main/java/uk/gov/di/config/RelyingPartyConfig.java
+++ src/main/java/uk/gov/di/config/RelyingPartyConfig.java
@@ -6,6 +6,7 @@ public class RelyingPartyConfig {
     public static final String OP_TOKEN_URL=getConfigValue("OP_TOKEN_URL","http://localhost:8080/token");
     public static final String OP_USERINFO_URL=getConfigValue("OP_USERINFO_URL","http://localhost:8080/userinfo");
     public static final String AUTH_CALLBACK_URL=getConfigValue("AUTH_CALLBACK_URL","http://localhost:8081/oidc/callback");
+    public static final String PORT=getConfigValue("RP_PORT","8081");
 
     private static String getConfigValue(String key, String defaultValue){
         var envValue = System.getenv(key);
