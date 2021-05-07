1801d949c9cbd2bafde5a695086d40602c58df9c
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-05-07 18:06:39 +0100

    BAU: Add a .NET stub RP implementation
    Uses IdentityModel certified library
         https://github.com/IdentityModel/IdentityModel.OidcClient

---
+++ dotnet/GDS.DigitalIdentity.Authentication.Stubs.RelyingParty/Controllers/PrivateController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GDS.DigitalIdentity.Authentication.Stubs.RelyingParty.Controllers
+{
+    public class PrivateController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
