86f394b91581f634748f50fdc071717f39890330
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-06-21 11:42:37 +0100

    BAU - Refactor Stub apps and pipeline
    - Create 2 instances of the Stub app. One to run in sandbox and one to run in the build environment.
    - Only store the base urls in parameter store and build the URL in the RelyingPartyConfig instance
    
    Co-authored-by: Daniel Besbrode <daniel.besbrode@digital.cabinet-office.gov.uk>

--- src/main/java/uk/gov/di/config/RelyingPartyConfig.java
+++ src/main/java/uk/gov/di/config/RelyingPartyConfig.java
@@ -1,14 +1,18 @@
 package uk.gov.di.config;
 
+import java.net.URI;
+import java.util.Optional;
+
 public class RelyingPartyConfig {
     public static final String CLIENT_ID= getConfigValue("CLIENT_ID", "some_client_id");
     public static final String CLIENT_SECRET = getConfigValue("CLIENT_SECRET", "password");
-    public static final String OP_AUTHORIZE_URL = getConfigValue("OP_AUTHORIZE_URL","http://localhost:8080/authorize");
-    public static final String OP_TOKEN_URL=getConfigValue("OP_TOKEN_URL","http://localhost:8080/token");
-    public static final String OP_USERINFO_URL=getConfigValue("OP_USERINFO_URL","http://localhost:8080/userinfo");
-    public static final String AUTH_CALLBACK_URL=getConfigValue("AUTH_CALLBACK_URL","http://localhost:8081/oidc/callback");
-    public static final String LOGOUT_URL=getConfigValue("LOGOUT_URL","http://localhost:8080/logout?redirectUri=http://localhost:8081/");
+    public static final String OP_AUTHORIZE_URL = getConfigValue("OP_BASE_URL","http://localhost:8080") + "/authorize";
+    public static final String OP_TOKEN_URL=getConfigValue("OP_BASE_URL","http://localhost:8080") + "/token";
+    public static final String OP_USERINFO_URL=getConfigValue("OP_BASE_URL","http://localhost:8080") + "/userinfo";
+    public static final String AUTH_CALLBACK_URL=getConfigValue("STUB_BASE_URL","http://localhost:8081") + "/oidc/callback";
+    public static final String LOGOUT_URL=generateLogoutUrl("OP_BASE_URL", "STUB_BASE_URL", "http://localhost:8080/logout?redirectUri=http://localhost:8081/");
     public static final String PORT=getConfigValue("RP_PORT","8081");
+
 
     private static String getConfigValue(String key, String defaultValue){
         var envValue = System.getenv(key);
@@ -17,5 +21,15 @@ public class RelyingPartyConfig {
         }
 
         return envValue;
+    }
+
+    private static String generateLogoutUrl(String baseUrlKey, String stubBaseUrlKey, String defaultValue) {
+        var baseUrlEnv = System.getenv(baseUrlKey);
+        var stubBaseUrlEnv = System.getenv(stubBaseUrlKey);
+
+        if (baseUrlEnv == null || stubBaseUrlEnv == null) {
+            return defaultValue;
+        }
+        return baseUrlEnv + "/logout?redirectUri=" + stubBaseUrlEnv;
     }
 }
