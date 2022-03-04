1d3c0a78818569dae553f4c7eb027fb6055292ee
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-03-04 12:35:02 +0000

    BAU: Refactor configuration into provider methods. Apply Spotless

--- src/main/java/uk/gov/di/config/RelyingPartyConfig.java
+++ src/main/java/uk/gov/di/config/RelyingPartyConfig.java
@@ -8,35 +8,66 @@ public class RelyingPartyConfig {
 
     private static final CfEnv cfEnv = new CfEnv();
 
-    public static final String SERVICE_NAME = getConfigValue("SERVICE_NAME", "Sample Government Service");
-    public static final String IDP_URL= getConfigValue("OP_BASE_URL", "https://api.build.auth.ida.digital.cabinet-office.gov.uk");
-    public static final String CLIENT_ID= getConfigValue("CLIENT_ID", "some_client_id");
-    public static final String AUTH_CALLBACK_URL=getCloudFoundryUri("http://localhost:8081") + "/oidc/authorization-code/callback";
-    public static final String POST_LOGOUT_REDIRECT_URL=getCloudFoundryUri("http://localhost:8081") + "/signed-out";
-    public static final String CLIENT_PRIVATE_KEY =getConfigValue("CLIENT_PRIVATE_KEY","PRIVATE-KEY");
-    public static final int PORT=getCloudfoundryPort(8081);
-    public static final String MY_ACCOUNT_URL = getConfigValue("MY_ACCOUNT_URL", "https://account-management.integration.auth.ida.digital.cabinet-office.gov.uk/");
+    public static final String SERVICE_NAME = serviceName();
+    public static final String IDP_URL = oidcProviderUrl();
+    public static final String CLIENT_ID = clientId();
+    public static final String AUTH_CALLBACK_URL = authCallbackUrl();
+    public static final String POST_LOGOUT_REDIRECT_URL = postLogoutRedirectUrl();
+    public static final String CLIENT_PRIVATE_KEY = clientPrivateKey();
+    public static final int PORT = getCloudfoundryPort();
+    public static final String MY_ACCOUNT_URL = accountManagementUrl();
 
-    private static String getConfigValue(String key, String defaultValue){
+    public static String authCallbackUrl() {
+        return getCloudFoundryUri() + "/oidc/authorization-code/callback";
+    }
+
+    public static String postLogoutRedirectUrl() {
+        return getCloudFoundryUri() + "/signed-out";
+    }
+
+    public static String clientPrivateKey() {
+        return getConfigValue("CLIENT_PRIVATE_KEY", "PRIVATE-KEY");
+    }
+
+    public static String accountManagementUrl() {
+        return getConfigValue(
+                "MY_ACCOUNT_URL",
+                "https://account-management.integration.auth.ida.digital.cabinet-office.gov.uk/");
+    }
+
+    public static String clientId() {
+        return getConfigValue("CLIENT_ID", "some_client_id");
+    }
+
+    public static String serviceName() {
+        return getConfigValue("SERVICE_NAME", "Sample Government Service");
+    }
+
+    public static String oidcProviderUrl() {
+        return getConfigValue(
+                "OP_BASE_URL", "https://api.build.auth.ida.digital.cabinet-office.gov.uk");
+    }
+
+    public static String getCloudFoundryUri() {
+        if (cfEnv.isInCf() && cfEnv.getApp().getUris().size() > 0) {
+            return format("https://{0}", cfEnv.getApp().getUris().get(0));
+        }
+        return "http://localhost:8081";
+    }
+
+    public static int getCloudfoundryPort() {
+        if (cfEnv.isInCf()) {
+            return 8080;
+        }
+        return 8081;
+    }
+
+    private static String getConfigValue(String key, String defaultValue) {
         var envValue = System.getenv(key);
-        if(envValue == null){
+        if (envValue == null) {
             return defaultValue;
         }
 
         return envValue;
-    }
-
-    private static String getCloudFoundryUri(String defaultValue) {
-        if (cfEnv.isInCf() && cfEnv.getApp().getUris().size() > 0) {
-            return format("https://{0}", cfEnv.getApp().getUris().get(0));
-        }
-        return defaultValue;
-    }
-
-    private static int getCloudfoundryPort(int defaultValue) {
-        if (cfEnv.isInCf()) {
-            return 8080;
-        }
-        return defaultValue;
     }
 }
