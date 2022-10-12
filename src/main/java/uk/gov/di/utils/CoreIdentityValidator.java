538e4c4c5966e9726a282443eea26b18da214230
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-10-12 17:22:37 +0100

    BAU: Parse and validate signed JWT with the EC key

--- src/main/java/uk/gov/di/utils/CoreIdentityValidator.java
+++ src/main/java/uk/gov/di/utils/CoreIdentityValidator.java
@@ -1,10 +1,13 @@
 package uk.gov.di.utils;
 
 import com.nimbusds.jose.Algorithm;
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.crypto.ECDSAVerifier;
 import com.nimbusds.jose.jwk.Curve;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jwt.SignedJWT;
 import uk.gov.di.config.RelyingPartyConfig;
 
 import java.security.KeyFactory;
@@ -12,6 +15,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.ECPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
+import java.text.ParseException;
 import java.util.Base64;
 import java.util.Optional;
 
@@ -20,6 +24,8 @@ public class CoreIdentityValidator {
     private final ECKey key;
 
     public enum Result {
+        VALID,
+        INVALID,
         NOT_VALIDATED
     }
 
@@ -53,7 +59,13 @@ public class CoreIdentityValidator {
     }
 
     public Result isValid(String jwt) {
-        return Result.NOT_VALIDATED;
+        try {
+            return SignedJWT.parse(jwt).verify(new ECDSAVerifier(this.key))
+                    ? Result.VALID
+                    : Result.INVALID;
+        } catch (JOSEException | ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     static class NoopCoreIdentityValidator extends CoreIdentityValidator {
