25e4f199040866c8a1e375d7cb97ee3cf239bcbe
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-10-12 17:21:07 +0100

    BAU: Split out a `NoopCoreIdentityValidator` based on env variable

--- src/main/java/uk/gov/di/utils/CoreIdentityValidator.java
+++ src/main/java/uk/gov/di/utils/CoreIdentityValidator.java
@@ -1,5 +1,7 @@
 package uk.gov.di.utils;
 
+import uk.gov.di.config.RelyingPartyConfig;
+
 public class CoreIdentityValidator {
 
     public enum Result {
@@ -7,10 +9,25 @@ public class CoreIdentityValidator {
     }
 
     public static CoreIdentityValidator createValidator() {
-        return new CoreIdentityValidator();
+        return RelyingPartyConfig.identitySigningPublicKey()
+                .map(CoreIdentityValidator::new)
+                .orElse(new NoopCoreIdentityValidator());
     }
+
+    private CoreIdentityValidator(Object jwt) {}
 
     public Result isValid(String jwt) {
         return Result.NOT_VALIDATED;
+    }
+
+    static class NoopCoreIdentityValidator extends CoreIdentityValidator {
+        private NoopCoreIdentityValidator() {
+            super(null);
+        }
+
+        @Override
+        public Result isValid(String jwt) {
+            return Result.NOT_VALIDATED;
+        }
     }
 }
