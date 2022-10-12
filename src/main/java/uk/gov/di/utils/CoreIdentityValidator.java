e92b1629f7c03bff88e574e7eefb6620188f4ac0
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-10-12 17:21:50 +0100

    BAU: Read env variable as ECKey

--- src/main/java/uk/gov/di/utils/CoreIdentityValidator.java
+++ src/main/java/uk/gov/di/utils/CoreIdentityValidator.java
@@ -1,8 +1,23 @@
 package uk.gov.di.utils;
 
+import com.nimbusds.jose.Algorithm;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.KeyUse;
 import uk.gov.di.config.RelyingPartyConfig;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Optional;
+
 public class CoreIdentityValidator {
+
+    private final ECKey key;
 
     public enum Result {
         NOT_VALIDATED
@@ -10,11 +25,32 @@ public class CoreIdentityValidator {
 
     public static CoreIdentityValidator createValidator() {
         return RelyingPartyConfig.identitySigningPublicKey()
+                .map(Base64.getDecoder()::decode)
+                .map(X509EncodedKeySpec::new)
+                .flatMap(
+                        spec -> {
+                            try {
+                                return Optional.ofNullable(
+                                        KeyFactory.getInstance("EC").generatePublic(spec));
+                            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+                                e.printStackTrace();
+                                return Optional.empty();
+                            }
+                        })
+                .map(ECPublicKey.class::cast)
+                .map(
+                        key ->
+                                new ECKey.Builder(Curve.P_256, key)
+                                        .keyUse(KeyUse.SIGNATURE)
+                                        .algorithm(new Algorithm(JWSAlgorithm.ES256.getName()))
+                                        .build())
                 .map(CoreIdentityValidator::new)
                 .orElse(new NoopCoreIdentityValidator());
     }
 
-    private CoreIdentityValidator(Object jwt) {}
+    private CoreIdentityValidator(ECKey key) {
+        this.key = key;
+    }
 
     public Result isValid(String jwt) {
         return Result.NOT_VALIDATED;
