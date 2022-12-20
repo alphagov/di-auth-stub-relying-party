2b02708904582eed324b36700462f348d97e9a4d
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2022-12-20 16:54:23 +0000

    BAU: Read environment variables from .env file on local startup

--- startup.sh
+++ startup.sh
@@ -4,5 +4,7 @@ set -eu
 echo "Building DI authentication Stub relying party"
 ./gradlew clean build
 
+export $(grep -v '^#' .env | xargs)
+
 echo "Starting DI authentication Stub relying party"
 ./gradlew run
