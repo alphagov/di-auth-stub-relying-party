f211315288b8a43600b86599c441403fb1a3e3e6
Aidan Comer <alhcomer@hotmail.co.uk>
2023-10-25 08:57:17 +0100

    AUT-1838: added sec headers
    fixed code smell
    
    applied spotless

--- src/main/java/uk/gov/di/handlers/ErrorHandler.java
+++ src/main/java/uk/gov/di/handlers/ErrorHandler.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import uk.gov.di.utils.ResponseHeaderHelper;
 import uk.gov.di.utils.ViewHelper;
 
 import java.util.HashMap;
@@ -22,7 +23,7 @@ public class ErrorHandler implements Route {
         var model = new HashMap<>();
         model.put("error", request.queryParams("error"));
         model.put("error_description", request.queryParams("error_description"));
-        response.header("Server", "govuk-sign-in-stub-rp");
+        ResponseHeaderHelper.setHeaders(response);
         return ViewHelper.render(model, "error.mustache");
     }
 }
